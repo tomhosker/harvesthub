2cb98ef861c7ca4e0fcad2c869803c75044a80a5
tomhosker <thosker@b-hiveinnovations.co.uk>
2019-10-28 15:16:36 +0000

    Revised table name checking functions.

--- sql/dothething.sql
+++ sql/dothething.sql
@@ -1,18 +1,4 @@
 -- Run me on the database for the HarvestHub app using:
---   heroku pg:psql --app harvesthub < create_drop.sql
-
--- Remember that PostgreSQL will changes the names of columns, tables, etc
--- to all lower case unless quotation marks are used.
-
-DROP TABLE IF EXISTS Grower;
-
-CREATE TABLE Grower
-(
-  code VARCHAR(999) PRIMARY KEY,
-  fullName VARCHAR(999)
-);
-
--- Run me on the database for the HarvestHub app using:
 --   heroku pg:psql --app harvesthub < dothething.sql
 
 -- Remember that PostgreSQL will changes the names of columns, tables, etc
